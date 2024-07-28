e0a6775b7fc3761fa3eebf20e881fc18dc107f85
xuehao03 <xuehao03@58.com>
2024-07-28 19:07:43 +0800

    实现简单的本地调用

---
+++ src/main/java/part1/common/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package part1.common.service;
+
+
+import part1.common.pojo.User;
+
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * @author xuehao
+ */
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUserByUserId(Integer id) {
+        System.out.println("客户端查询了" + id + "的用户");
+        // 模拟从数据库中取用户的行为
+        Random random = new Random();
+        return User.builder()
+                .userName(UUID.randomUUID()
+                .toString())
+                .id(id)
+                .sex(random.nextBoolean()).build();
+    }
+
+    @Override
+    public Integer insertUserId(User user) {
+        System.out.println("插入数据成功" + user.getUserName());
+        return user.getId();
+    }
+}
---
+++ src/main/java/part1/server/handler/impl/SimpleRpcServer.java
@@ -0,0 +1,39 @@
+package part1.server.handler.impl;
+
+import lombok.AllArgsConstructor;
+
+import part1.server.handler.RpcServer;
+import part1.server.handler.work.WorkThread;
+import part1.server.provider.ServiceProvider;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author xuehao
+ */
+
+@AllArgsConstructor
+public class SimpleRpcServer implements RpcServer {
+    private ServiceProvider serviceProvider;
+    @Override
+    public void start(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port);){
+            System.out.println("服务启动");
+            while (true) {
+                // 如果没有连接，堵塞在这。
+                Socket socket = serverSocket.accept();
+                // 有连接，创建一个新的线程执行任务
+                new Thread(new WorkThread(socket, serviceProvider)).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
